2b7c551b645fd58ecbdcd2df1ec6d2a6e9a1ab60
Arya Reais-Parsi <himself@aryaparsi.com>
2018-04-27 19:31:48 -0700

    colossal brain fart

--- hardware/src/video/accelerator.v
+++ hardware/src/video/accelerator.v
@@ -55,53 +55,5 @@ module accelerator #(
     reg color_reg;
     reg TX_running;
 
-    wire complete = (x_curr == x_target && y_curr == y_target);
-
-    assign RX_ready = ~TX_running;
-
-    assign XL_wr_en = TX_running;
-    assign XL_wr_data = color_reg;
-    assign XL_wr_addr = x_curr * pixel_width + y_curr; 
-    
-    //2-phase operation
-    //Phase 1: determine pixel
-    //Phase 2: write pixel
-    //On a clock edge, pixel K is written to the memory, and pixel K+1 is calculated and registered
-    always @(posedge clk) begin
-        if (TX_running) begin
-            if (complete) begin
-                TX_running <= 1'b0;
-            end else if (x_curr == x_target && px < 0) begin    //I don't think this can happen but...
-                TX_running <= 1'b0;
-            end else begin //set up phase 2
-                if (px < 0) begin
-                    px <= px + pk_step_L;
-                    x_curr <= x_curr + 1;
-                    y_curr <= y_curr;
-                end else begin
-                    px <= px + pk_step_U;
-                    x_curr <= x_curr;
-                    y_curr <= y_curr + 1;
-                end
-            end
-            //end state
-        end else if (RX_valid) begin
-            TX_running <= 1'b1;
-
-            x_curr <= x0;
-            y_curr <= y0;
-
-            x_target <= x1;
-            y_target <= y1;
-
-            pk_step_L <= 2 * (y1 - y0);
-            pk_step_U <= 2 * (y1 - y0) - 2 * (x1 - x0);
-
-            px <= 2 * (y1 - y0) - (x1 - x0); //initial
-
-            color_reg <= color;
-        end else begin
-            TX_running <= 1'b0;
-        end
-    end
+    // No cheating.
 endmodule
